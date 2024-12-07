3f0822aa945f5a72275ac079c48ad63195a55da4
Woo Jin Kim <effect105@hotmail.com>
2024-12-08 08:06:05 +0900

    init

--- backend/src/obj/Debug/net9.0/src.AssemblyInfo.cs
+++ backend/src/obj/Debug/net9.0/src.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("src")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fa53e615254f9dc4d202c83b8d439fc75b5ed2a")]
 [assembly: System.Reflection.AssemblyProductAttribute("src")]
 [assembly: System.Reflection.AssemblyTitleAttribute("src")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
